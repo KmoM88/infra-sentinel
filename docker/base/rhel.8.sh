94b470b78ca1f299f2682c08c470a93a61f07e4c
Federico Rossi <federico.rossi@creativa77.com.ar>
2026-04-11 23:37:32 -0300

    add versions for rhel

--- docker/base/rhel.8.sh
+++ docker/base/rhel.8.sh
@@ -7,8 +7,6 @@ echo "No dev tools listed in RHEL 8"
 dnf update -y && dnf upgrade -y
 dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 dnf install -y 'dnf-command(config-manager)'
-dnf config-manager --set-enabled ubi-8-powertools-rpms
-dnf config-manager --set-enabled ubi-8-appstream-rpms
 dnf config-manager --add-repo https://mirror.stream.centos.org/8-stream/BaseOS/x86_64/os/
 dnf config-manager --add-repo https://mirror.stream.centos.org/8-stream/AppStream/x86_64/os/
 dnf config-manager --add-repo https://mirror.stream.centos.org/8-stream/CRB/x86_64/os/
