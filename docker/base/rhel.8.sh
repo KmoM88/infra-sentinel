c0e09565b6555c4eb1d753efa605056471b1a3ad
Federico Rossi <federico.rossi@creativa77.com.ar>
2026-04-11 23:42:36 -0300

    add versions for rhel

--- docker/base/rhel.8.sh
+++ docker/base/rhel.8.sh
@@ -7,9 +7,9 @@ echo "No dev tools listed in RHEL 8"
 dnf update -y && dnf upgrade -y
 dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 dnf install -y 'dnf-command(config-manager)'
-dnf config-manager --add-repo https://mirror.stream.centos.org/8-stream/BaseOS/x86_64/os/
-dnf config-manager --add-repo https://mirror.stream.centos.org/8-stream/AppStream/x86_64/os/
-dnf config-manager --add-repo https://mirror.stream.centos.org/8-stream/PowerTools/x86_64/os/
+dnf config-manager --add-repo https://vault.stream.centos.org/8-stream/BaseOS/x86_64/os/
+dnf config-manager --add-repo https://vault.stream.centos.org/8-stream/AppStream/x86_64/os/
+dnf config-manager --add-repo https://vault.stream.centos.org/8-stream/PowerTools/x86_64/os/
 rpm --import https://www.centos.org/keys/RPM-GPG-KEY-CentOS-Official
 dnf clean all
 dnf makecache
