a7f56c14908e827dc1ad6e96ea4eaad287671066
Federico Rossi <federico.rossi@creativa77.com.ar>
2026-04-11 23:24:05 -0300

    add versions for rhel

--- docker/base/rhel.8.sh
+++ docker/base/rhel.8.sh
@@ -3,3 +3,15 @@
 set -e
 
 echo "No dev tools listed in RHEL 8"
+
+dnf update -y && dnf upgrade -y
+dnf install -y 'dnf-command(config-manager)' https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+dnf config-manager --set-enabled ubi-8-powertools-rpms
+dnf config-manager --set-enabled ubi-8-appstream-rpms
+dnf config-manager --add-repo https://mirror.stream.centos.org/8-stream/BaseOS/x86_64/os/
+dnf config-manager --add-repo https://mirror.stream.centos.org/8-stream/AppStream/x86_64/os/
+dnf config-manager --add-repo https://mirror.stream.centos.org/8-stream/CRB/x86_64/os/
+rpm --import https://www.centos.org/keys/RPM-GPG-KEY-CentOS-Official
+dnf makecache
+rpm -e --nodeps openssl-fips-provider-so || true
+dnf update -y --allowerasing
--- docker/base/rhel.9.sh
+++ docker/base/rhel.9.sh
@@ -22,3 +22,15 @@ dnf install -y \
   python3-setuptools \
   python3-vcstool \
   wget
+
+dnf update -y && dnf upgrade -y
+dnf install -y 'dnf-command(config-manager)' epel-release
+dnf config-manager --set-enabled ubi-9-codeready-builder-rpms
+dnf config-manager --set-enabled ubi-9-appstream-rpms
+dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/
+dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/AppStream/x86_64/os/
+dnf config-manager --add-repo https://mirror.stream.centos.org/9-stream/CRB/x86_64/os/
+rpm --import https://www.centos.org/keys/RPM-GPG-KEY-CentOS-Official
+dnf makecache
+rpm -e --nodeps openssl-fips-provider-so || true
+dnf update -y --allowerasing
